c12be24fd7a9799fbb0d60dd700a4f12842457ca
rccuam <racc@correo.azc.uam.mx>
2018-04-03 23:41:34 -0500

    segment tree, pop_back y neutro
    nueva función y más bonito :)

--- Estructuras de datos/segment_tree.cpp
+++ Estructuras de datos/segment_tree.cpp
@@ -1,11 +1,12 @@
+#include <climits>
 #include <numeric>
 #include <vector>
 
 template<typename T, typename F>
 class segment_tree {
 public:
-   segment_tree(F f = { })
-   : pisos(1), funcion(f) {
+   segment_tree(F f, T n)
+   : pisos(1), funcion(f), neutro(std::move(n)) {
    }
 
    int size( ) const {
@@ -32,35 +33,45 @@ public:
       }
    }
 
-   T query(int ini, int fin, const T& v) const {
-      return query(ini, fin, v, pisos.size( ) - 1);
+   void pop_back( ) {
+      for (int p = 0; p < pisos.size( ); ++p) {
+         pisos[p].pop_back( );
+         if (pisos[p].size( ) % 2 == 0) {
+            break;
+         }
+      }
+   }
+
+   T query(int ini, int fin) const {
+      return query(ini, fin, pisos.size( ) - 1);
    }
 
 private:
-   T query(int ini, int fin, const T& v, int p) const {
+   T query(int ini, int fin, int p) const {
       if (ini == fin) {
-         return v;
+         return neutro;
       } else {
          int grupo = 1 << p, xi = ini / grupo + bool(ini % grupo), xf = fin / grupo;
          if (xi < xf && xf <= pisos[p].size( )) {
-            return funcion(std::accumulate(pisos[p].begin( ) + xi, pisos[p].begin( ) + xf, v, funcion), funcion(query(ini, xi * grupo, v, p - 1), query(xf * grupo, fin, v, p - 1)));
+            return funcion(std::accumulate(pisos[p].begin( ) + xi, pisos[p].begin( ) + xf, neutro, funcion), funcion(query(ini, xi * grupo, p - 1), query(xf * grupo, fin, p - 1)));
          } else {
-            return query(ini, fin, v, p - 1);
+            return query(ini, fin, p - 1);
          }
       }
    }
 
    std::vector<std::vector<T>> pisos;
    F funcion;
+   T neutro;
 };
 
 template<typename T, typename F>
-segment_tree<T, F> make_segment_tree(F f = { }) {
-   return segment_tree<T, F>(f);
+segment_tree<T, F> make_segment_tree(F f, T neutro) {
+   return segment_tree<T, F>(f, std::move(neutro));
 }
 
 int main( ) {
-   auto s = make_segment_tree<int>(std::max<int>);
+   auto s = make_segment_tree<int>(std::max<int>, INT_MIN);
    for (int i = 0; i < 50; ++i) {
       s.push_back(i);
    }

16f42640a40a131b76bab0a988191eefaa7727f2
Rodrigo <racc@azc.uam.mx>
2019-12-03 09:55:12 -0600

    fenwick D
    1

--- Cadenas/knuth_morris_pratt.cpp
+++ Cadenas/knuth_morris_pratt.cpp
@@ -3,37 +3,30 @@
 #include <random>
 #include <vector>
 
-std::vector<std::size_t> preprocesa(const std::string& p)
-{
+std::vector<std::size_t> preprocesa(const std::string& s) {
    std::vector<std::size_t> b = { std::size_t(-1) };
-   for (std::size_t i = 0, j = -1; i < p.size( ); ++i) {
-      while (j != -1 && p[j] != p[i]) {
+   for (std::size_t i = 0, j = -1; i < s.size( ); ++i) {
+      while (j != -1 && s[i] != s[j]) {
          j = b[j];
       }
-
       b.push_back(++j);
    }
-
    return b;
 }
 
-std::size_t busca(const std::string& p, const std::string& t, const std::vector<std::size_t>& b)
-{
+std::size_t busca(const std::string& s, const std::string& t, const std::vector<std::size_t>& b) {
    for (std::size_t i = 0, j = 0; i < t.size( ); ++i) {
-      while (j != -1 && p[j] != t[i]) {
+      while (j != -1 && t[i] != s[j]) {
          j = b[j];
       }
-
-      if (++j == p.size( )) {
-         return i + 1 - p.size( );
+      if (++j == s.size( )) {
+         return i + 1 - s.size( );
       }
    }
-
    return std::string::npos;
 }
 
-int main( )
-{
+int main( ) {
    std::string s = "abbabaabababaaabaaababbaaaababbabaaaba";
    std::string p = "bbaaaababba";
 

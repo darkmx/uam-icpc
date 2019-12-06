a6af7027c2394759504e39f80599eee823dae7ee
rccuam <racc@correo.azc.uam.mx>
2019-12-05 20:24:03 -0600

    fix nombres
    va

--- Estructuras de datos/fenwick_tree_D.cpp
+++ Estructuras de datos/fenwick_tree_D.cpp
@@ -12,7 +12,7 @@ public:
    template<typename... T>
    void add(int i, const T&... s) {
       for (i += 1; i < mem_.size( ); i += (i & -i)) {
-         mem_[i].actualiza(s...);
+         mem_[i].add(s...);
       }
    }
 
@@ -20,7 +20,7 @@ public:
    int prefix(int i, const T&... s) {
       int res = 0;
       for (i += 1; i != 0; i -= (i & -i)) {
-         res += mem_[i].cuenta(s...);
+         res += mem_[i].prefix(s...);
       }
       return res;
    }
@@ -32,11 +32,11 @@ private:
 template<>
 class fenwick_tree<0> {
 public:
-   void actualiza(int v) {
+   void add(int v) {
       v_ += v;
    }
 
-   int cuenta( ) {
+   int prefix( ) {
       return v_;
    }
 
@@ -47,11 +47,11 @@ private:
 int main( ) {
    prueba_1d: {
       fenwick_tree<1> arbol(10);
-      arbol.actualiza(2, +1);
-      arbol.actualiza(4, +1);
+      arbol.add(2, +1);
+      arbol.add(4, +1);
 
       for (int i = 0; i < 10; ++i) {
-         std::cout << arbol.cuenta(i) << " ";
+         std::cout << arbol.prefix(i) << " ";
       }
       std::cout << "\n";
    }
@@ -60,12 +60,12 @@ int main( ) {
 
    prueba_2d: {
       fenwick_tree<2> arbol(10, 10);
-      arbol.actualiza(2, 2, +1);
-      arbol.actualiza(4, 4, +1);
+      arbol.add(2, 2, +1);
+      arbol.add(4, 4, +1);
 
       for (int i = 0; i < 10; ++i) {
          for (int j = 0; j < 10; ++j) {
-            std::cout << arbol.cuenta(i, j) << " ";
+            std::cout << arbol.prefix(i, j) << " ";
          }
          std::cout << "\n";
       }

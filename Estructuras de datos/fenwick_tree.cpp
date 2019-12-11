aad0913e75c27be06afafe0dd6c393b16c012f1e
Rodrigo <racc@azc.uam.mx>
2019-12-11 09:32:59 -0600

    mejora fenwick
    1

--- Estructuras de datos/fenwick_tree.cpp
+++ Estructuras de datos/fenwick_tree.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <vector>
 
+template<typename T>
 class fenwick_tree {
 public:
    fenwick_tree(int n)
    : mem_(n + 1) {
    }
 
-   void add(int i, int d) {
+   T current(int i) {
+      return prefix(i) - (i != 0 ? prefix(i - 1) : 0);
+   }
+
+   void replace(int i, const T& v) {
+      advance(i, v - current(i));
+   }
+
+   void advance(int i, const T& d) {
       for (i += 1; i < mem_.size( ); i += (i & -i)) {
          mem_[i] += d;
       }
    }
 
-   int prefix(int i) {
-      int res = 0;
+   T prefix(int i) {
+      T res = 0;
       for (i += 1; i != 0; i -= (i & -i)) {
          res += mem_[i];
       }
@@ -22,16 +31,23 @@ public:
    }
 
 private:
-   std::vector<int> mem_;
+   std::vector<T> mem_;
 };
 
 int main( ) {
-   fenwick_tree arbol(10);    // inicialmente todo en cero
+   fenwick_tree<int> arbol(10);    // inicialmente todo en cero
 
    for (int i = 0; i < 10; ++i) {
-      arbol.add(i, +1);
+      arbol.advance(i, +1);
    }
    for (int i = 0; i < 10; ++i) {
-      std::cout << i << ": " << arbol.prefix(i) << "\n";
+      std::cout << i << ": " << arbol.current(i) << " (" << arbol.prefix(i) << ")\n";
    }
+   std::cout << "\n";
+
+   arbol.replace(4, 5);
+   for (int i = 0; i < 10; ++i) {
+      std::cout << i << ": " << arbol.current(i) << " (" << arbol.prefix(i) << ")\n";
+   }
+   std::cout << "\n";
 }

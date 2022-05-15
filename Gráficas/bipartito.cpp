08ac3c98b88fe2c5095f4959ec8e26f389403baa
Rodrigo Castro <rcc.dark@gmail.com>
2022-05-15 08:19:11 -0500

    mejorando bipartito
    mejorando bipartito

--- Gráficas/bipartito.cpp
+++ Gráficas/bipartito.cpp
@@ -1,31 +1,57 @@
 #include <algorithm>
+#include <queue>
+#include <tuple>
 #include <vector>
 
-bool aumenta(int a, const std::vector<int> adyacencia_a[], std::vector<int>& arista_b, std::vector<bool>& visto_b) {
-   for (int b : adyacencia_a[a]) {
-      if (!visto_b[b]) {
-         visto_b[b] = true;
-         if (arista_b[b] == -1 || aumenta(arista_b[b], adyacencia_a, arista_b, visto_b)) {
-            arista_b[b] = a;
+namespace bipartito {
+   bool aumenta(int a, std::vector<int> adyacencia_a[], std::vector<int>& nivel_a, std::vector<int>& pareja_a, std::vector<int>& pareja_b, bool visto_a[]) {
+      visto_a[a] = true;
+      for (int b : adyacencia_a[a]) {
+         if (pareja_b[b] == -1 || !visto_a[pareja_b[b]] && nivel_a[a] < nivel_a[pareja_b[b]] && aumenta(pareja_b[b], adyacencia_a, nivel_a, pareja_a, pareja_b, visto_a)) {
+            pareja_a[a] = b;
+            pareja_b[b] = a;
             return true;
          }
       }
+      return false;
    }
-   return false;
-}
 
-std::pair<std::vector<int>, int> acoplamiento(const std::vector<int> adyacencia_a[], int tam_a, int tam_b) {
-   std::vector<int> arista_b(tam_b, -1);
-   for (int a = 0; a < tam_a; ++a) {
-      std::vector<bool> visto_b(tam_b, false);
-      aumenta(a, adyacencia_a, arista_b, visto_b);
+   auto calcula(std::vector<int> adyacencia_a[], int n, int m) {
+      std::vector<int> nivel_a(n), pareja_a(n, -1), pareja_b(m, -1);
+      for (;;) {
+         std::queue<int> cola;
+         for (int a = 0; a < n; ++a) {
+            if (pareja_a[a] == -1) {
+               nivel_a[a] = 0, cola.push(a);
+            } else {
+               nivel_a[a] = -1;
+            }
+         }
+
+         for (; !cola.empty( ); cola.pop( )) {
+            int a = cola.front( );
+            for (int b : adyacencia_a[a]) {
+               if (pareja_b[b] != -1 && nivel_a[pareja_b[b]] < 0) {
+                  nivel_a[pareja_b[b]] = nivel_a[a] + 1;
+                  cola.push(pareja_b[b]);
+               }
+            }
+         }
+
+         bool mejora = false, visto_a[n] = { };
+         for (int a = 0; a < n; ++a) {
+            mejora |= (pareja_a[a] == -1 && aumenta(a, adyacencia_a, nivel_a, pareja_a, pareja_b, visto_a));
+         }
+         if (!mejora) {
+            int cardinalidad = n - std::count(pareja_a.begin( ), pareja_a.end( ), -1);
+            return std::tuple(std::move(pareja_a), std::move(pareja_b), cardinalidad);
+         }
+      }
    }
-   int cardinalidad = tam_b - std::count(arista_b.begin( ), arista_b.end( ), -1);
-   return { std::move(arista_b), cardinalidad };
-}
+};
 
 int main( ) {
    // acoplamiento bipartito: |A| = tam_a, |B| = tam_b
    // Los identificadores de vértices van de 0 -> tam_a - 1 en A y 0 -> tam_b - 1 en B.
-   // La función regresa un arreglo de tam_b enteros que contienen los vértices de A con los que se conectan los vértices de B (-1 si un vértice de B no tiene pareja). También regresa la cardinalidad del emparejamiento
+   // La función regresa un arreglo de tam_b enteros cola contienen los vértices de A con los cola se conectan los vértices de B (-1 si un vértice de B no tiene pareja). También regresa la cardinalidad del emparejamiento
 }

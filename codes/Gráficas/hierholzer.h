52a5484b329648c92b251438106823f515475721
Rodrigo Castro <rcc.dark@gmail.com>
2025-07-27 18:15:27 -0600

    Update hierholzer.h

--- codes/Gráficas/hierholzer.h
+++ codes/Gráficas/hierholzer.h
@@ -6,44 +6,18 @@
  * Complejidad: $O(n + m)$
  */
 #include <algorithm>
-#include <iomanip>
-#include <list>
-#include <queue>
+#include <utility>
 #include <set>
 #include <vector>
-#include <math.h>
 
-struct punto {
-   double x, y;
-};
-
-struct arco {
-   int x, y;
-};
-
-double factor_windy(punto p1, punto p2) {
-   return (p1.x <= p2.x ? 1 / 1.5 : 2);
-}
-
-double distancia_windy(punto p1, punto p2) {
-   return hypot(factor_windy(p1, p2) * (p1.x - p2.x), p1.y - p2.y);
-}
-
-struct por_costo {
-   const punto* arr;
-   bool operator()(const arco& a, const arco& b) const {
-      return std::tuple(distancia_windy(arr[a.x], arr[a.y]), a.x, a.y) < std::tuple(distancia_windy(arr[b.x], arr[b.y]), b.x, b.y);
-   }
-};
-
-std::vector<int> hierholzer(int inicial, std::vector<std::set<arco, por_costo>>& adyacencia) {
+std::vector<int> hierholzer(int inicial, std::vector<std::set<std::pair<int, int>>>& adyacencia) {
    std::vector<int> pila = { inicial }, res;
    do {
       int actual = pila.back( );
       if (!adyacencia[actual].empty( )) {
          auto [origen, destino] = *adyacencia[actual].begin( );
          adyacencia[actual].erase(adyacencia[actual].begin( ));
-         adyacencia[destino].erase(arco(destino, actual));
+         adyacencia[destino].erase(std::pair(destino, actual));
          pila.push_back(destino);
       } else {
          res.push_back(actual);
